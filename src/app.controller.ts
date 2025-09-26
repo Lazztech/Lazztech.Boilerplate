682d10ac4d859483de5664eab79369eb44ec34ad
Gian Lazzarini <gianlazzarini@gmail.com>
2025-09-25 21:43:11 -0700

    Set up mvc with handlebars, tailwind, daisyui, htmx, and config service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private messages: { user: string; message: string }[] = [];
+
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getHello(): any {
+    return {
+      isProd: this.configService.get('NODE_ENV') == 'prod',
+      appName: this.configService.get('APP_NAME') as string,
+    };
+  }
+
+  @Get('messages')
+  getMessages(): any {
+    return this.messages
+      .map((msg) => `<p><strong>${msg.user}:</strong> ${msg.message}</p>`)
+      .join('');
+  }
+
+  @Post('message')
+  async postMessages(@Body() body: any) {
+    const message = body.message as string;
+    this.messages.push({ user: 'user', message });
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // Simple response logic
+    const responseMessage = `Received your message: ${message}`;
+    this.messages.push({ user: 'Assistant', message: responseMessage });
+
+    return `
+      <p><strong>Assistant:</strong> ${responseMessage}</p>
+    `;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+      validationSchema: Joi.object({
+        APP_NAME: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
